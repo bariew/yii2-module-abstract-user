a881ef33d87bfcb3fcbbacec9bbb4ad9a8556d53
Pavel Bariev <bariew@yandex.ru>
2016-06-29 17:19:54 +0400

    fix

--- UserModule.php
+++ UserModule.php
@@ -1,11 +1,22 @@
 <?php
+/**
+ * UserModule class file.
+ * @copyright (c) 2016, Pavel Bariev
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ */
 
 namespace bariew\userAbstractModule;
 use Yii;
-use bariew\userAbstractModule\models\User;
 use yii\web\Application;
 
-class Module extends \yii\base\Module
+/**
+ * Description.
+ *
+ * Usage:
+ * @author Pavel Bariev <bariew@yandex.ru>
+ *
+ */
+class UserModule extends \yii\base\Module
 {
     public $params = [
         'emailConfirm' => false,
@@ -15,7 +26,7 @@ class Module extends \yii\base\Module
 
     public function init()
     {
-        $this->params['menu'] = (!\bariew\userAbstractModule\Module::hasUser())
+        $this->params['menu'] = (!static::hasUser())
             ? [
                   'label'    => 'Login', 
                   'url' => ['/user/default/login']
